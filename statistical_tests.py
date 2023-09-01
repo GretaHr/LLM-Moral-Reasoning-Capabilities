9b6f29d0a5a590a8fa4de30ee4d2732be57af536
Greta Hristozova <46601579+GretaHr@users.noreply.github.com>
2023-09-01 23:04:37 +0300

    initial

---
+++ statistical_tests.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from sklearn.metrics import f1_score, accuracy_score, mean_absolute_error, log_loss
+import tensorflow as tf
+from tensorflow import keras
+from keras.losses import binary_crossentropy
+
+# scenarios ordered by ascending human.response
+
+y_gold = [0.145161290, 0.174603175, 0.250000000, 0.255813954, 0.270833333, 0.274193548, 0.418604651, 0.472972973, 0.675675676, 0.702702703, 0.703333333, 0.779069767, 0.791666667, 0.840909091, 0.866666667, 0.883720930]
+
+y_yes_no = [0, 0.125, 0.6, 0, 0.571428571, 0, 0, 0.6, 1, 1, 0.428571429, 0, 0.857142857, 1, 0.142857143, 0.75]
+# exclude @[0] 0, only 1 decisive run
+
+y_eth_com = [0, 0, 0.1, 0.125, 0, 0, 0.111111111, 0.1, 0, 0.222222222, 0.1, 0, 0.111111111, 0, 0, 0.555555556]
+
+y_step_vic = [0, 0, 0, 0, 0, 0, 0.1, 0, 0, 0.5, 0, 0, 0.833333333, 0.6, 0, 1]
+# exclude @[9] 0.5, only 2 decisive runs
+
+y_step_fal = [0, 0, 0, 0, 0, 0, 0, 0, 0.1, 0.5, 0, 0, 0, 0, 0, 0]
+
+cases = [y_yes_no, y_eth_com, y_step_vic, y_step_fal]
+
+# ratio to 0/1 label
+def to_binary(results):
+    return [1 if r > 0.5 else 0 for r in results]
+
+
+# f1 score
+def f1(y_true, y_pred):
+    return f1_score(to_binary(y_true), to_binary(y_pred), zero_division=1.0)
+
+# accuracy
+def acc(y_true, y_pred):
+    return accuracy_score(to_binary(y_true), to_binary(y_pred))
+
+#TODO
+def cons(y_true, y_pred):
+    # false negative/all false
+    # is faster to compute by hand
+    pass
+    
+# mean absolute error
+def mae(y_true, y_pred):
+    return mean_absolute_error(y_true, y_pred)
+
+# binary cross entropy
+def bce(y_true, y_pred):
+    bce = tf.keras.losses.BinaryCrossentropy(from_logits=False)
+    return bce(to_binary(y_true), y_pred).numpy()
+
+
+def evaluate():
+
+    for y_pred in cases:
+        y_true = y_gold
+
+        # exclude first scenario
+        if y_pred == y_yes_no:
+            y_pred = y_yes_no[1:]
+            y_true = y_gold[1:]
+
+        # exclude ninth scenario
+        elif y_pred == y_step_vic:
+            y_pred = y_step_vic[:9] + y_step_vic[10:]
+            y_true = y_gold[:9] + y_gold[10:]
+
+        # TODO add cons(gold, y)
+        scores = [f1(y_true, y_pred), acc(y_true, y_pred), mae(y_true, y_pred), bce(y_true, y_pred)]
+        
+        print("\t".join([str(s) for s in scores]))
+
+
+if __name__ == '__main__':
+    evaluate()
